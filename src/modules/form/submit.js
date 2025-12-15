c6d9cd1d0c1392ef24940132cc4790e4d66eb611
Kevin Lopes <kevinlopes2304@gmail.com>
2025-12-15 15:36:33 -0300

    feat(form): implement form submission handling and date input initialization

---
+++ src/modules/form/submit.js
@@ -0,0 +1,18 @@
+import dayjs from "dayjs";
+
+const form = document.querySelector("form");
+const selectedDate = document.getElementById("date");
+
+//Date atual para o input
+const inputToday = dayjs(new Date()).format("YYYY-MM-DD");
+
+//Carrega a data atual e define a data minima como sendo a data atual
+selectedDate.value = inputToday;
+selectedDate.min = inputToday;
+
+form.onsubmit = (event) => {
+  //Previne o comportamento padrão de carregar a pagina.
+  event.preventDefault();
+
+  console.log("ENVIADO!!");
+};
