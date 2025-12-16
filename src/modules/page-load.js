a1cd5a729f305949b9e5b5989f91f04d85f00802
Kevin Lopes <kevinlopes2304@gmail.com>
2025-12-16 14:52:58 -0300

    feat(load): replace load module with page-load and implement hoursLoad function for scheduling

---
+++ src/modules/page-load.js
@@ -0,0 +1,5 @@
+import { scheculesDay } from "./schedules/load.js";
+
+document.addEventListener("DOMContentLoaded", function () {
+  scheculesDay();
+});
---
+++ src/modules/schedules/load.js
@@ -0,0 +1,12 @@
+import { hoursLoad } from "../form/hours-load";
+
+//Seleciona o input de data
+const selectedDate = document.getElementById("date");
+
+export function scheculesDay() {
+  //Obtem a data selecionada pelo usuário
+  const date = selectedDate.value;
+
+  //Renderiza as horas disponíveis na página de agendamentos
+  hoursLoad({ date });
+}
