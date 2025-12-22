602b0444049b1eb7e1e15363a35714a984d39c8a
Kevin Lopes <kevinlopes2304@gmail.com>
2025-12-22 15:17:02 -0300

    feat(schedule): enhance scheduling functionality by implementing schedule display and fetching by day

--- src/modules/schedules/load.js
+++ src/modules/schedules/load.js
@@ -1,5 +1,6 @@
 import { hoursLoad } from "../form/hours-load";
 import { scheduleFetchByDay } from "../../services/schedule-fetch-by-day";
+import { schedulesShow } from "./show.js";
 
 //Seleciona o input de data
 const selectedDate = document.getElementById("date");
@@ -10,6 +11,9 @@ export async function schedulesDay() {
 
   //Busca na API os agendamentos
   const dailySchedules = await scheduleFetchByDay({ date });
+
+  //Exibe os agendamentos
+  schedulesShow({ dailySchedules });
 
   //Renderiza as horas disponíveis na página de agendamentos
   hoursLoad({ date });
