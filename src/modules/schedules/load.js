7194c338338d71caf83a3a8c1bda8076b4a0ab11
Kevin Lopes <kevinlopes2304@gmail.com>
2025-12-22 14:44:49 -0300

    feat(schedule): make schedulesDay function asynchronous to fetch daily schedules

--- src/modules/schedules/load.js
+++ src/modules/schedules/load.js
@@ -1,11 +1,15 @@
 import { hoursLoad } from "../form/hours-load";
+import { scheduleFetchByDay } from "../../services/schedule-fetch-by-day";
 
 //Seleciona o input de data
 const selectedDate = document.getElementById("date");
 
-export function schedulesDay() {
+export async function schedulesDay() {
   //Obtem a data selecionada pelo usuário
   const date = selectedDate.value;
+
+  //Busca na API os agendamentos
+  const dailySchedules = await scheduleFetchByDay({ date });
 
   //Renderiza as horas disponíveis na página de agendamentos
   hoursLoad({ date });
