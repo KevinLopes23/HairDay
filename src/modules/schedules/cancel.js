c785ba2709d0c05453e2495bfac0b2c22a6408ef
Kevin Lopes <kevinlopes2304@gmail.com>
2025-12-23 16:53:18 -0300

    feat(schedule): add cancel functionality for scheduling periods

---
+++ src/modules/schedules/cancel.js
@@ -0,0 +1,19 @@
+const periods = document.querySelectorAll(".period");
+
+//Gera evento de click para cada lista(manhã, tarde e noite)
+periods.forEach((period) => {
+  //Captura o evento de click na lista
+  period.addEventListener("click", (event) => {
+    if (event.target.classList.contains("cancel-icon")) {
+      //Obtém a li pai do elemento clicado.
+      const item = event.target.closest("li");
+      const id = item.dataset.id;
+
+      if(id){
+        const isConfirm = confirm("Tem certeza que deseja cancelar esse agendamento?");
+        
+        if(isConfirm){}
+      }
+    }
+  });
+});
